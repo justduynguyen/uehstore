d6ebc885d0c3ac9ed9f92ff63c2365a48e80cd2b
TITANCORP\duy.nguyenth <duy.nguyenth@titancorpvn.com>
2023-08-15 19:47:17 +0700

    .

--- public/custom.js
+++ public/custom.js
@@ -1,14 +1,14 @@
-document.addEventListener('gesturestart', function (e) {
-	e.preventDefault();
-	document.body.style.zoom = 0.99;
-});
+// document.addEventListener('gesturestart', function (e) {
+// 	e.preventDefault();
+// 	document.body.style.zoom = 0.99;
+// });
 
-document.addEventListener('gesturechange', function (e) {
-	e.preventDefault();
+// document.addEventListener('gesturechange', function (e) {
+// 	e.preventDefault();
 
-	document.body.style.zoom = 0.99;
-});
-document.addEventListener('gestureend', function (e) {
-	e.preventDefault();
-	document.body.style.zoom = 1;
-});
+// 	document.body.style.zoom = 0.99;
+// });
+// document.addEventListener('gestureend', function (e) {
+// 	e.preventDefault();
+// 	document.body.style.zoom = 1;
+// });
